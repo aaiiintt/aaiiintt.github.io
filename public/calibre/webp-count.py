e114d173e18825539b09eeb2233c83296412e72c
AAIIINTT <aiin@aaiiintt.xyz>
2024-04-04 01:38:01 +0100

    remove

--- public/calibre/webp-count.py
+++
@@ -1,9 +0,0 @@
-import os
-
-directory = 'images'
-webp_files = [f for f in os.listdir(directory) if f.endswith('.webp')]
-
-filenames = ', '.join(f'"{file}"' for file in webp_files)
-
-with open('URLS.txt', 'w') as file:
-    file.write(filenames)
